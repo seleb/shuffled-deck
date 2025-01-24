125cf98a9341105f466c912234f36ee32777a5e2
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2025-01-23 19:50:19 -0500

    chore: formatting

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
+const suits = ['spades', 'hearts', 'diamonds', 'clubs'];
+const values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13, 14];
 
-const suits = ['spades','hearts','diamonds','clubs'];
-const values = [1,2,3,4,5,6,7,8,9,10,11,13,14];
-
-const suitOffsets = ['spades','hearts','diamonds','clubs'].reduce((result, suit, idx) => ({
-	...result,
-	[suit]: idx*16,
-}), {});
+const suitOffsets = ['spades', 'hearts', 'diamonds', 'clubs'].reduce(
+	(result, suit, idx) => ({
+		...result,
+		[suit]: idx * 16,
+	}),
+	{}
+);
 const names = {
 	1: 'ace',
 	2: 'two',
@@ -27,43 +29,43 @@ const names = {
 // 'suit-value'
 // 'back'
 // 'joker'
-function getSymbol({
-	suit,
-	value,
-}) {
+function getSymbol({ suit, value }) {
 	const unique = {
-		'back': 0x1F0A0,
-		'joker': 0x1F0BF,
+		back: 0x1f0a0,
+		joker: 0x1f0bf,
 	};
 	if (unique[value]) {
 		return String.fromCodePoint(unique[value]);
 	}
 	const start = unique.back;
-	return String.fromCodePoint(start + suitOffsets[suit] + parseInt(value,10));
+	return String.fromCodePoint(start + suitOffsets[suit] + parseInt(value, 10));
 }
 
 function shuffle() {
 	const ref = [];
 	const jokers = parseInt(document.getElementById('jokers').value, 10);
-	new Array(jokers).fill(0).forEach(() => ref.push({
-		suit: '',
-		value: 'joker',
-	}));
-	suits.forEach(suit => values.forEach(value => ref.push({
-		suit,
-		value,
-	})));
-	
+	new Array(jokers).fill(0).forEach(() =>
+		ref.push({
+			suit: '',
+			value: 'joker',
+		})
+	);
+	suits.forEach(suit =>
+		values.forEach(value =>
+			ref.push({
+				suit,
+				value,
+			})
+		)
+	);
+
 	const deck = [];
-	while(ref.length) {
-		deck.push(ref.splice(Math.floor(Math.random()*ref.length), 1)[0]);
+	while (ref.length) {
+		deck.push(ref.splice(Math.floor(Math.random() * ref.length), 1)[0]);
 	}
 	const el = document.getElementById('deck');
 	el.innerHTML = '';
-	deck.forEach(({
-		suit,
-		value,
-	}) => {
+	deck.forEach(({ suit, value }) => {
 		const li = document.createElement('li');
 		function flipUp() {
 			li.innerText = getSymbol({ suit, value });
@@ -73,7 +75,7 @@ function shuffle() {
 		}
 		function flipDown() {
 			li.innerText = getSymbol({ value: 'back' });
-			li.className= 'back';
+			li.className = 'back';
 			li.title = '';
 			li.onclick = flipUp;
 		}
@@ -83,6 +85,6 @@ function shuffle() {
 }
 
 const elBtnShuffle = document.querySelector('button');
-if (!elBtnShuffle) throw new Error("could not find elements");
+if (!elBtnShuffle) throw new Error('could not find elements');
 elBtnShuffle.addEventListener('click', shuffle);
 shuffle();
